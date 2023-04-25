f900d006b11c0344114f484db207724f3b011e42
Harel Natan <harelnatan7@gmail.com>
2023-04-25 08:59:29 +0300

    Added base elements

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.scss']
+})
+export class RecipesListComponent {
+recipes: Recipe[] = [
+  new Recipe('test', 'test', 'url')
+]
+
+}
